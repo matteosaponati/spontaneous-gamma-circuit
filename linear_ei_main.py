a55d96bc80ee285400453ab13814005f38e3be2f
Matteo Saponati <matteosaponati@gmail.com>
2021-11-02 12:32:22 +0100

    command prompt

--- linear_ei_main.py
+++ linear_ei_main.py
@@ -1,19 +1,28 @@
 import numpy as np
+import sys
+import ast
 import linear_ei_funs as funs
 
-beta1 = 1.9449
-beta2 = -.9801
-A = [1,-1.1,1,-.84]
-time = 1000
+"""
+PARAMETERS
+beta1
+beta2
+A
+time (total number of timestep for num simulation)
+"""
+beta1 = float(sys.argv[1]) if len(sys.argv) > 1 else 1.9449
+beta2 = float(sys.argv[2]) if len(sys.argv) > 2 else -.9801
+A = ast.literal_exal(sys.argv[3]) if len(sys.argv) > 3 else [1,-1.1,1,-.84]
+time = int(sys.argv[4]) if len(sys.argv) > 4 else 1000
 
 """
 NUMERICAL SOLUTIONS
 find the solution numerically for the AR(2) model
 compute affine transformation in the (E,I) space
 """
-ar,E,I,eps = funs.num_solution(time,A,beta1,beta2)
+ar,E,I,eps = funs.num_solution(beta1,beta2,A,time)
 
 'save'
 np.save('num_solution_ei',[E,I])
-np.save('num_solution_ar',[ar])
+np.save('num_solution_ar',ar)
 
