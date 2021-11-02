df731600adaae01fa91ec07da831fc9ac5d6d22b
Matteo Saponati <matteosaponati@gmail.com>
2021-11-02 12:43:34 +0100

    minor

--- linear_ei_main.py
+++ linear_ei_main.py
@@ -12,7 +12,7 @@ time (total number of timestep for num simulation)
 """
 beta1 = float(sys.argv[1]) if len(sys.argv) > 1 else 1.9449
 beta2 = float(sys.argv[2]) if len(sys.argv) > 2 else -.9801
-A = ast.literal_exal(sys.argv[3]) if len(sys.argv) > 3 else [1,-1.1,1,-.84]
+A = ast.literal_eval(sys.argv[3]) if len(sys.argv) > 3 else [1,-1.1,1,-.84]
 time = int(sys.argv[4]) if len(sys.argv) > 4 else 1000
 
 """
